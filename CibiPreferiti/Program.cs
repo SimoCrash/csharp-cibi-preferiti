9d19cd5a194ca24c0718cfb3e90bc97482ddf437
SimoCrash <simone.crescenzi@hotmail.com>
2023-03-15 17:47:42 +0100

    top 3

--- CibiPreferiti/Program.cs
+++ CibiPreferiti/Program.cs
@@ -1,10 +1,11 @@
 ﻿string[] cibiPreferiti = { "Pizza", "Biscotti", "Pancakes", "Hamburger", "Tortellini", "Bacon", "Ciambelle" };
 
-//Console.WriteLine("Classifica:");
+Console.WriteLine("I miei cibi preferiti sono:");
 
 for (int i = 0; i < cibiPreferiti.Length; i++)
 {
-    Console.WriteLine(cibiPreferiti[i]);
+    
+Console.WriteLine(cibiPreferiti[i]);
 }
-
-Console.WriteLine("I miei cibi preferiti sono: " + cibiPreferiti.Length);
+Console.WriteLine("I miei cibi preferiti sono totalmente: " + cibiPreferiti.Length);
+Console.WriteLine("La top 3 in ordine è : " + cibiPreferiti[0] + ", " + cibiPreferiti[5] + ", " + cibiPreferiti[1]);
