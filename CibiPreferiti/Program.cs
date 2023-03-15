4824f7e648a7d6775b5fb2473c661792ef016f06
SimoCrash <simone.crescenzi@hotmail.com>
2023-03-15 17:57:33 +0100

    meno preferito

--- CibiPreferiti/Program.cs
+++ CibiPreferiti/Program.cs
@@ -10,3 +10,4 @@ Console.WriteLine(cibiPreferiti[i]);
 Console.WriteLine("I miei cibi preferiti sono totalmente: " + cibiPreferiti.Length);
 Console.WriteLine("La posizione in classifica in ordine è : " + cibiPreferiti[0] + ", " + cibiPreferiti[5] + ", " + cibiPreferiti[1] + ", " + cibiPreferiti[4] + ", " + cibiPreferiti[2] + ", " + cibiPreferiti[6] + ", " + cibiPreferiti[3]);
 Console.WriteLine("Il mio cibo top è la " + cibiPreferiti[0] + ", ma 10 a 0 proprio");
+Console.WriteLine("Il meno preferito è l'" + cibiPreferiti[3] + ", non che faccia schifo!");
