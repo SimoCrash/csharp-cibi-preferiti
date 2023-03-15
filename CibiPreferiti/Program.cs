fa4182d3065cea291cc39168ade23c40d5a0f174
SimoCrash <simone.crescenzi@hotmail.com>
2023-03-15 20:52:15 +0100

    BONUS

--- CibiPreferiti/Program.cs
+++ CibiPreferiti/Program.cs
@@ -4,10 +4,19 @@ Console.WriteLine("I miei cibi preferiti sono:");
 
 for (int i = 0; i < cibiPreferiti.Length; i++)
 {
+    Console.WriteLine(cibiPreferiti[i]);
     
-Console.WriteLine(cibiPreferiti[i]);
 }
 Console.WriteLine("I miei cibi preferiti sono totalmente: " + cibiPreferiti.Length);
 Console.WriteLine("La posizione in classifica in ordine è : " + cibiPreferiti[0] + ", " + cibiPreferiti[5] + ", " + cibiPreferiti[1] + ", " + cibiPreferiti[4] + ", " + cibiPreferiti[2] + ", " + cibiPreferiti[6] + ", " + cibiPreferiti[3]);
 Console.WriteLine("Il mio cibo top è la " + cibiPreferiti[0] + ", ma 10 a 0 proprio");
 Console.WriteLine("Il meno preferito è l'" + cibiPreferiti[3] + ", non che faccia schifo!");
+
+if (cibiPreferiti.Length % 2 == 0)
+{
+    Console.WriteLine(cibiPreferiti[cibiPreferiti.Length / 2]);
+}
+else
+{
+    Console.WriteLine(cibiPreferiti[cibiPreferiti.Length / 2]);
+}
