a1e7d7396c33e331fa30509cb1d0ebe57cac9b87
SimoCrash <simone.crescenzi@hotmail.com>
2023-03-15 17:30:33 +0100

    print array

--- CibiPreferiti/Program.cs
+++ CibiPreferiti/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] cibiPreferiti = { "pizza", "biscotti", "pancakes", "hamburger", "tortellini", "bacon", "ciambelle" };
+
+for(int i = 0; i< cibiPreferiti.Length; i++)
+{
+    //cibiPreferiti[i];
+}
+for (int i = 0; i < cibiPreferiti.Length; i++)
+{
+    Console.WriteLine(cibiPreferiti[i]);
+}
